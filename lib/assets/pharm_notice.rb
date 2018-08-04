4da8684799c884f9e4240314980d4913ee821b92
EC2 Default User <ec2-user@ip-172-31-27-178.us-east-2.compute.internal>
2018-08-04 09:47:29 +0000

    경영대학빼고 다 완료

--- lib/assets/pharm_notice.rb
+++ lib/assets/pharm_notice.rb
@@ -2,6 +2,7 @@ require 'nokogiri'
 require 'net/http'
 require 'active_support/all'
 require 'uri'
+require 'open-uri'
 
 
 list_uri = URI("http://pharm.skku.edu/board/board.jsp")
@@ -23,6 +24,42 @@ list_uri = URI("http://pharm.skku.edu/board/board.jsp")
     list_doc = Nokogiri::HTML(list_req.body)
     links = list_doc.css('tr a')
     links.each do |link|
-        link['href'].split('goView(')
+        id = link['href'].scan(/\d+/).first
+        notice_doc = Nokogiri::HTML(open("http://pharm.skku.edu/board/view.jsp?curNum=#{id}"))
+        title = notice_doc.css('tr th').text
+        writer = notice_doc.css("td.spoqa").text.scan(/｜(.+)날짜/).first.first
+        date = notice_doc.css("td.spoqa").text.scan(/날짜｜(.+)조회수/).first.first.to_datetime
+        view = notice_doc.css("td.spoqa").text.scan(/조회수｜(.+)/).first.first
+        content = notice_doc.css('div#contents')
+        
+        Notice.create(
+            :title => title,
+            :writer => writer,
+            :date => date,
+            :content => content,
+            :view => view,
+            :scrap_count => 0, #TODO: 물어보기
+            :group_id => 16
+            )
+            
+        files = notice_doc.css('tr:nth-of-type(3) > td > a')
+        files.each do |file|
+            if (file['href'] =~ /^http/)
+                file_link = file['href']
+            else
+                file_link = "http://pharm.skku.edu/board/"+file['href']
+            end
+             Attached.create(:link => file_link, :name => file.text, :notice_id => Notice.last.id)
+        end
+        
+        images = notice_doc.css('div#contents img')
+        images.each do |image|
+            if (image['src'] =~ /^http/)
+                img_link = image['src']
+            else
+                img_link = "http://pharm.skku.edu"+image['src']
+            end
+            Image.create(:link => img_link, :notice_id => Notice.last.id)
+        end
     end
 end
